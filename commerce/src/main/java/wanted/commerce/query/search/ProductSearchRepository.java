8501b922956fe3eec78cf5293d66aa9da7b96a92
소중한 <sojunghan49@gmail.com>
2025-05-22 15:48:04 +0900

    [DEV] elasticsearch query model 동기화 추가

---
+++ commerce/src/main/java/wanted/commerce/query/search/ProductSearchRepository.java
@@ -0,0 +1,8 @@
+package wanted.commerce.query.search;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductSearchRepository extends ElasticsearchRepository<ProductSearchDocument, Long> {
+}

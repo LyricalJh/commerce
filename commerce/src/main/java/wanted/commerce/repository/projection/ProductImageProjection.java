7f596769e05252ff2c4cf9c71b8d5c44050527ee
소중한 <sojunghan49@gmail.com>
2025-05-22 22:20:21 +0900

    [DEV] query model 동기화 추가

---
+++ commerce/src/main/java/wanted/commerce/repository/projection/ProductImageProjection.java
@@ -0,0 +1,11 @@
+package wanted.commerce.repository.projection;
+
+public interface ProductImageProjection {
+    Long getId();
+    String getUrl();
+    String getAltText();
+    Boolean getIsPrimary();
+    Integer getDisplayOrder();
+    Long getProductId();
+    Long getOptionId();
+}
